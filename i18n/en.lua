14c86dd218b2a5d02cb4d40d3259e3c1253e6cd6
Sharlikran <danielhmpdx16@gmail.com>
2022-10-30 10:31:39 -0700

    Version: 5.0.0

---
+++ i18n/en.lua
@@ -0,0 +1,24 @@
+-------------------------------------------------------------------------------
+-- ENGLISH
+-------------------------------------------------------------------------------
+
+-- Bindings
+
+ZO_CreateStringId("SI_BINDING_NAME_RAFFLEMANAGER_TOGGLE", "Show/Hide RaffleManager")
+
+-- Tabs
+
+ZO_CreateStringId("SI_RAFFLEMANAGER_TITLE_EXPORT", "Export")
+ZO_CreateStringId("SI_RAFFLEMANAGER_TITLE_MESSAGE", "Message")
+ZO_CreateStringId("SI_RAFFLEMANAGER_TITLE_CONFIRM", "Confirm")
+
+-- Buttons
+ZO_CreateStringId("SI_RAFFLEMANAGER_EXPORT_MAIL_BUTTON", "Export Mailbox")
+ZO_CreateStringId("SI_RAFFLEMANAGER_EXPORT_ROSTER_BUTTON", "Export Guild Roster")
+ZO_CreateStringId("SI_RAFFLEMANAGER_EXPORT_GUILD_BUTTON", "Export Guild Bank")
+ZO_CreateStringId("SI_RAFFLEMANAGER_RELOAD_BUTTON", "Save Data/Reload UI")
+
+ZO_CreateStringId("SI_RAFFLEMANAGER_COMPOSE_BUTTON_SEND", "Send")
+ZO_CreateStringId("SI_RAFFLEMANAGER_COMPOSE_BUTTON_CANCEL", "Cancel")
+ZO_CreateStringId("SI_RAFFLEMANAGER_COMPOSE_BUTTON_PAUSE", "Pause")
+ZO_CreateStringId("SI_RAFFLEMANAGER_COMPOSE_BUTTON_CONTINUE", "Continue")
